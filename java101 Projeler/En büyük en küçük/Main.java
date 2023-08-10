7b9b3ec5e37974efd4039e79a56706e23ebd7ea9
Ahmet Hakan Şenel <115705325+ahmetHakanSenel@users.noreply.github.com>
2023-08-10 19:17:03 +0300

    Add files via upload

---
+++ java101 Projeler/En büyük en küçük/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Kaç tane sayı gireceksiniz: ");
+        int n = scanner.nextInt();
+
+        int enBuyuk = 0;
+        int enKucuk = 0;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + ". sayıyı girin: ");
+            int sayi = scanner.nextInt();
+
+            if (sayi > enBuyuk) {
+                enBuyuk = sayi;
+            }
+
+            if (sayi < enKucuk) {
+                enKucuk = sayi;
+            }
+        }
+
+        System.out.println("En büyük sayı: " + enBuyuk);
+        System.out.println("En küçük sayı: " + enKucuk);
+    }
+}
